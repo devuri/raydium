41532333ffe4e82425a78ba28f28c53407726777
Uriel <devuri@users.noreply.github.com>
2024-09-09 16:24:15 -0500

    Update bootstrap.php

--- bootstrap.php
+++ bootstrap.php
@@ -21,7 +21,9 @@ if ( file_exists( \dirname( __FILE__ ) . '/vendor/autoload.php' ) ) {
  *
  * @var string|null RAYDIUM_ENVIRONMENT_TYPE The environment type, can be null to use the .env file setup.
  */
-define( 'RAYDIUM_ENVIRONMENT_TYPE', null );
+if ( ! defined( 'RAYDIUM_ENVIRONMENT_TYPE' ) ) {
+	define( 'RAYDIUM_ENVIRONMENT_TYPE', null );
+}
 
 /**
  * Start and bootstrap the web application.
