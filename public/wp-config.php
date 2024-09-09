d05e26aa6f01b4498ce5da17afdd3c008429d226
Uriel <devuri@users.noreply.github.com>
2024-09-09 16:27:03 -0500

    fix: Update wp-config.php

--- public/wp-config.php
+++ public/wp-config.php
@@ -1,21 +1,8 @@
 <?php
 
-/*
- * This is the bootstrap file for the web application.
- *
- * It loads the necessary files and sets up the environment for the application to run.
- * This includes initializing the Composer autoloader, which is used to load classes and packages.
- *
- * The application uses Composer to manage dependencies and packages including WordPress themes and plugins, which allows you to easily add, update, and remove packages as needed.
- * To install or update packages, you can use the `composer` command line.
- *
- * IMPORTANT: Do NOT modify this file unless you are sure of what you are doing.
- * Any changes you make to this file may affect the behavior of the entire application and cause errors or unexpected behavior.
- * If you are unsure about how to modify the application, please refer to the documentation or seek assistance from a qualified developer.
- *
- * To modify the setup, please refer to the documentation for instructions.
- *
- */
+
+/* That's all, stop editing! Happy publishing. */
+
 if ( file_exists(__DIR__ . '/../bootstrap.php') ) {
 	require_once __DIR__ . '/../bootstrap.php';
 }
