e1efee504e112655f68817a6aece91ca608150f3
uri <devuri@users.noreply.github.com>
2025-03-07 16:16:32 -0500

    Update wp-config.php

--- public/wp-config.php
+++ public/wp-config.php
@@ -8,19 +8,6 @@ if (file_exists(dirname(__DIR__) . '/vendor/autoload.php')) {
     exit('Cannot find the vendor autoload file.');
 }
 
-/**
- * For developers: WordPress debugging mode.
- *
- * Change this to true to enable the display of notices during development.
- * It is strongly recommended that plugin and theme developers use WP_DEBUG
- * in their development environments.
- *
- * For information on other constants that can be used for debugging,
- * visit the documentation.
- *
- * @link https://developer.wordpress.org/advanced-administration/debug/debug-wordpress/
- */
-define( 'WP_DEBUG', false );
 
 /* That's all, stop editing! Happy publishing. */
 
